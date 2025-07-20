3b73134f44c27373f93600eb7c7576e7ddc395db
bondlazy <96534706+bondlazy@users.noreply.github.com>
2025-07-20 17:36:19 +0800

    Update main.py
    change hifini.com to hifini.com.cn

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def bark(device_key, title, content, bark_icon):
 
 
 def signV1(cookie):
-    url = 'https://www.hifini.com/sg_sign.htm'
+    url = 'https://www.hifini.com.cn/sg_sign.htm'
     headers = {
         'Cookie': cookie,
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
@@ -58,17 +58,17 @@ def signV2(cookie, sign):
     dynamicKey = generateDynamicKey()
     encryptedSign = simpleEncrypt(sign, dynamicKey)
 
-    url = "https://hifini.com/sg_sign.htm"
+    url = "https://hifini.com.cn/sg_sign.htm"
     params = {'sign': encryptedSign}
     payload = urlencode(params)
     headers = {
-        'authority': 'hifini.com',
+        'authority': 'hifini.com.cn',
         'accept': 'text/plain, */*; q=0.01',
         'accept-language': 'zh-CN,zh;q=0.9',
         'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
         'cookie': cookie,
-        'origin': 'https://hifini.com',
-        'referer': 'https://hifini.com/sg_sign.htm',
+        'origin': 'https://hifini.com.cn',
+        'referer': 'https://hifini.com.cn/sg_sign.htm',
         'sec-ch-ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',
@@ -85,17 +85,17 @@ def signV2(cookie, sign):
     return response.text
 
 def signV3(cookie, sign):
-    url = "https://hifini.com/sg_sign.htm"
+    url = "https://hifini.com.cn/sg_sign.htm"
     params = {'sign': sign}
     payload = urlencode(params)
     headers = {
-        'authority': 'hifini.com',
+        'authority': 'hifini.com.cn',
         'accept': 'text/plain, */*; q=0.01',
         'accept-language': 'zh-CN,zh;q=0.9',
         'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
         'cookie': cookie,
-        'origin': 'https://hifini.com',
-        'referer': 'https://hifini.com/sg_sign.htm',
+        'origin': 'https://hifini.com.cn',
+        'referer': 'https://hifini.com.cn/sg_sign.htm',
         'sec-ch-ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',
